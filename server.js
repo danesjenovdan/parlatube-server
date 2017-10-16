e6d28b1e843367cef45a03ecec7402872c6e9e29
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-10-16 21:03:31 +0200

    added soocenje url

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.get('/playlist/:playlistId', async ( req, res ) => {
 
 app.use('/', express.static(`${__dirname}/parlatube/dist`));
 app.use('/izseki', express.static(`${__dirname}/parlatube/dist`));
+app.use('/soocenje/:videoId', express.static(`${__dirname}/parlatube/dist`));
 
 
 app.use('/images', express.static(`${__dirname}/og_renders`));
